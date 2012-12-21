b8d20acb2952693984aa55361cec7b3b9040ce65
Chris Hamant <chris@hamant.net>
2012-12-21 03:48:36 -0500

    annexation

--- jruby/aliases.zsh
+++ jruby/aliases.zsh
@@ -1,2 +1,6 @@
 # Quick switch to jruby
 alias j='rbenv shell jruby-1.7.1'
+alias jng='export JRUBY_OPTS="--ng"'
+alias jngclear='export JRUBY_OPTS=""'
+alias ngup='ruby --ng-server &!'
+alias ngdn='ng ng-stop'
